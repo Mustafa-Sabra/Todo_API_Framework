ec4bf8a01937b76875df985ca5da6f05177e7ca1
Mustafa Sabra <mustafasabra44@gmail.com>
2026-01-15 23:22:39 +0200

    Adding the basic todo tests

---
+++ src/test/java/com/qacart/todo/testcases/TodoTests.java
@@ -0,0 +1,96 @@
+package com.qacart.todo.testcases;
+
+import io.restassured.http.ContentType;
+import org.hamcrest.CoreMatchers;
+import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static org.hamcrest.Matchers.equalTo;
+
+public class TodoTests {
+
+    @Test
+    public void shouldBeAbleToAddTodo() {
+        String body = "{\n" +
+                "  \"isCompleted\": false,\n" +
+                "  \"item\": \"first item\"\n" +
+                "}";
+
+        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5NjhiN2M0MTliYjRjMDAxNTc5MjNmMCIsImZpcnN0TmFtZSI6Im11c3RhZmEiLCJsYXN0TmFtZSI6InNhYnJhIiwiaWF0IjoxNzY4NDg2NDYzfQ.gUevV0KpEv9gAeVGFYcKFcWNIkMfcH9vsLsIGpMb1FU";
+
+
+        given()
+                .baseUri("https://qacart-todo.herokuapp.com")
+                .contentType(ContentType.JSON)
+                .body(body)
+                .auth().oauth2(token)
+                .when()
+                .post("/api/v1/tasks")
+                .then()
+                .log().all()
+                .assertThat().statusCode(201)
+                .assertThat().body("item", equalTo("first item"))
+                .assertThat().body("isCompleted", equalTo(false));
+    }
+
+    @Test
+    public void shouldNotBeAbleToAddTodoIfIsCompletedIsMissing() {
+        String body = "{\n" +
+                "  \"item\": \"first item\"\n" +
+                "}";
+
+        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5NjhiN2M0MTliYjRjMDAxNTc5MjNmMCIsImZpcnN0TmFtZSI6Im11c3RhZmEiLCJsYXN0TmFtZSI6InNhYnJhIiwiaWF0IjoxNzY4NDg2NDYzfQ.gUevV0KpEv9gAeVGFYcKFcWNIkMfcH9vsLsIGpMb1FU";
+
+        given()
+                .baseUri("https://qacart-todo.herokuapp.com")
+                .contentType(ContentType.JSON)
+                .body(body)
+                .auth().oauth2(token)
+                .when()
+                .post("/api/v1/tasks")
+                .then()
+                .log().all()
+                .assertThat().statusCode(400)
+                .assertThat().body("message", equalTo("\"isCompleted\" is required"));
+    }
+
+    @Test
+    public void shouldBeAbleToGetATodoWithId() {
+        String taskId = "696950de19bb4c0015792e4f";
+
+        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5NjhiN2M0MTliYjRjMDAxNTc5MjNmMCIsImZpcnN0TmFtZSI6Im11c3RhZmEiLCJsYXN0TmFtZSI6InNhYnJhIiwiaWF0IjoxNzY4NDg2NDYzfQ.gUevV0KpEv9gAeVGFYcKFcWNIkMfcH9vsLsIGpMb1FU";
+
+        given()
+                .baseUri("https://qacart-todo.herokuapp.com")
+                .contentType(ContentType.JSON)
+                .auth().oauth2(token)
+                .when()
+                .get("/api/v1/tasks/" + taskId)
+                .then()
+                .log().all()
+                .assertThat().statusCode(200)
+                .assertThat().body("isCompleted",equalTo( false))
+                .assertThat().body("item", equalTo("first item"));
+    }
+
+    @Test
+    public void shouldBeAbleToDeleteATodo() {
+        String taskId = "696950de19bb4c0015792e4f";
+
+        String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5NjhiN2M0MTliYjRjMDAxNTc5MjNmMCIsImZpcnN0TmFtZSI6Im11c3RhZmEiLCJsYXN0TmFtZSI6InNhYnJhIiwiaWF0IjoxNzY4NDg2NDYzfQ.gUevV0KpEv9gAeVGFYcKFcWNIkMfcH9vsLsIGpMb1FU";
+
+        given()
+                .baseUri("https://qacart-todo.herokuapp.com")
+                .contentType(ContentType.JSON)
+                .auth().oauth2(token)
+                .when()
+                .delete("/api/v1/tasks/" + taskId)
+                .then()
+                .log().all()
+                .assertThat().statusCode(200)
+                .assertThat().body("isCompleted",equalTo( false))
+                .assertThat().body("item", equalTo("first item"));
+    }
+
+
+}
