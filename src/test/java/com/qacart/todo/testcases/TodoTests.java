78595ef018e58e3232f4d57ca6a9660b86f68dd5
Mustafa Sabra <mustafasabra44@gmail.com>
2026-01-16 03:00:56 +0200

    Add User and Todo Pojo class and Serialization

--- src/test/java/com/qacart/todo/testcases/TodoTests.java
+++ src/test/java/com/qacart/todo/testcases/TodoTests.java
@@ -1,5 +1,6 @@
 package com.qacart.todo.testcases;
 
+import com.qacart.todo.models.Todo;
 import io.restassured.http.ContentType;
 import org.hamcrest.CoreMatchers;
 import org.testng.annotations.Test;
@@ -11,10 +12,7 @@ public class TodoTests {
 
     @Test
     public void shouldBeAbleToAddTodo() {
-        String body = "{\n" +
-                "  \"isCompleted\": false,\n" +
-                "  \"item\": \"first item\"\n" +
-                "}";
+        Todo todo = new Todo(false, "new item");
 
         String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5NjhiN2M0MTliYjRjMDAxNTc5MjNmMCIsImZpcnN0TmFtZSI6Im11c3RhZmEiLCJsYXN0TmFtZSI6InNhYnJhIiwiaWF0IjoxNzY4NDg2NDYzfQ.gUevV0KpEv9gAeVGFYcKFcWNIkMfcH9vsLsIGpMb1FU";
 
@@ -22,29 +20,27 @@ public class TodoTests {
         given()
                 .baseUri("https://qacart-todo.herokuapp.com")
                 .contentType(ContentType.JSON)
-                .body(body)
+                .body(todo)
                 .auth().oauth2(token)
                 .when()
                 .post("/api/v1/tasks")
                 .then()
                 .log().all()
                 .assertThat().statusCode(201)
-                .assertThat().body("item", equalTo("first item"))
+                .assertThat().body("item", equalTo("new item"))
                 .assertThat().body("isCompleted", equalTo(false));
     }
 
     @Test
     public void shouldNotBeAbleToAddTodoIfIsCompletedIsMissing() {
-        String body = "{\n" +
-                "  \"item\": \"first item\"\n" +
-                "}";
+        Todo todo = new Todo("new item");
 
         String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5NjhiN2M0MTliYjRjMDAxNTc5MjNmMCIsImZpcnN0TmFtZSI6Im11c3RhZmEiLCJsYXN0TmFtZSI6InNhYnJhIiwiaWF0IjoxNzY4NDg2NDYzfQ.gUevV0KpEv9gAeVGFYcKFcWNIkMfcH9vsLsIGpMb1FU";
 
         given()
                 .baseUri("https://qacart-todo.herokuapp.com")
                 .contentType(ContentType.JSON)
-                .body(body)
+                .body(todo)
                 .auth().oauth2(token)
                 .when()
                 .post("/api/v1/tasks")
@@ -56,7 +52,7 @@ public class TodoTests {
 
     @Test
     public void shouldBeAbleToGetATodoWithId() {
-        String taskId = "696950de19bb4c0015792e4f";
+        String taskId = "69698cc119bb4c001579303d";
 
         String token = "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpZCI6IjY5NjhiN2M0MTliYjRjMDAxNTc5MjNmMCIsImZpcnN0TmFtZSI6Im11c3RhZmEiLCJsYXN0TmFtZSI6InNhYnJhIiwiaWF0IjoxNzY4NDg2NDYzfQ.gUevV0KpEv9gAeVGFYcKFcWNIkMfcH9vsLsIGpMb1FU";
 
@@ -70,7 +66,7 @@ public class TodoTests {
                 .log().all()
                 .assertThat().statusCode(200)
                 .assertThat().body("isCompleted",equalTo( false))
-                .assertThat().body("item", equalTo("first item"));
+                .assertThat().body("item", equalTo("new item"));
     }
 
     @Test
