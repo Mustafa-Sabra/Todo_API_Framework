e0aeae98eb62942fae8a4ee88fc5d5a3e2815f5b
Mustafa Sabra <mustafasabra44@gmail.com>
2026-01-16 20:26:38 +0200

    Read Environment from maven command line

--- src/test/java/com/qacart/todo/base/Specs.java
+++ src/test/java/com/qacart/todo/base/Specs.java
@@ -7,10 +7,30 @@ import static io.restassured.RestAssured.given;
 
 public class Specs {
 
+    public static String getEnv()
+    {
+        String env = System.getProperty("env", "PRODUCTION");
+        String baseURI;
+
+        switch (env)
+        {
+            case "PRODUCTION":
+                baseURI = "https://qacart-todo.herokuapp.com";
+            break;
+            case "LOCAL":
+                baseURI = "https://localhost:8080";
+            break;
+            default:
+                throw new RuntimeException("Environment is not supported");
+        }
+
+        return baseURI;
+    }
+
         public static RequestSpecification getRequestSpecs()
         {
             return given()
-                    .baseUri("https://qacart-todo.herokuapp.com")
+                    .baseUri(getEnv())
                     .contentType(ContentType.JSON)
                     .log().all();
         }
